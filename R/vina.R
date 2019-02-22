e0e3dee57e5ebb451d779f6f5e2d6a1cc4d90b84
ahmohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-02-22 17:02:05 +1000

    vina function documentation and example

--- R/vina.R
+++ R/vina.R
@@ -1,3 +1,33 @@
+
+#' R interface for Autodock Vina
+#'
+#' This is an R interface to allow calling Autodock Vina from whithin R. The package compile Vina
+#' and its dependencies upon installation.
+#'
+#' @rawNamespace useDynLib(libboost_system); useDynLib(libboost_thread); useDynLib(libboost_filesystem); useDynLib(libboost_program_options); useDynLib(libboost_date_time); useDynLib(autodockr)
+#' @author Ahmed Mohamed \email{mohamed@@kuicr.kyoto-u.ac.jp}
+#' @name autodockr-package
+#' @aliases autodockr
+#' @docType package
+#'
+NULL
+
+
+#' Run Autodock Vina 
+#'
+#' @param ligand_name filepath for PDBQT file containing ligand
+#' @param rigid_name filepath for PDBQT file containing target
+#' @param flex_name filepath for PDBQT file containing flex
+#' @param out_name filepath where the output mode will be written
+#'
+#' @return No return as the results as written to file
+#' @export
+#'
+#' @examples
+#' ligand_path = system.file("extdata", "ligand.pdbqt", package="autodockr")
+#' rigid_path = system.file("extdata", "target.pdbqt", package="autodockr")
+#' vina(ligand_name=ligand_path, rigid_name=rigid_path)
+#'
 vina <- function(ligand_name, rigid_name=NULL, flex_name=NULL, out_name=NULL) {
   if(is.null(rigid_name))
     rigid_name = character(0)
