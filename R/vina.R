9350903a42c47e5989dad505706fdc891993006c
ahmohamed <mohamed@kuicr.kyoto-u.ac.jp>
2019-02-22 16:08:15 +1000

    vina R function accepts filename arguments

--- R/vina.R
+++ R/vina.R
@@ -1,1 +1,14 @@
-vina <- function() {.C("vina", PACKAGE="autodockr")}
+vina <- function(ligand_name, rigid_name=NULL, flex_name=NULL, out_name=NULL) {
+  if(is.null(rigid_name))
+    rigid_name = character(0)
+
+  if(is.null(flex_name))
+    flex_name = character(0)
+
+  if(is.null(out_name))
+    out_name = character(0)
+
+  .C("vina",
+    rigid_name, flex_name, ligand_name, out_name,
+  PACKAGE="autodockr")
+}
